d44a73f7a83ea1581978ead16dbe97e665c0d850
gorbadil <ersin.mutlu@outlook.com.tr>
2023-06-21 00:43:17 +0300

    starting files

---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h1>My Main Content</h1>
+      <p>More Main Content</p>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
